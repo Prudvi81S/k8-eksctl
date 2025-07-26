bf7f42e1e232ebc3e38c04848c188de8402a5eb7
prudviraj <prudvirajvemu316@gmail.com>
2025-07-26 11:37:31 +0530

    user profile

--- workstation.sh
+++ workstation.sh
@@ -40,11 +40,11 @@ usermod -aG docker ec2-user
 VALIDATE $? "Docker installation"
 
 # eksctl
-curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-sudo mv /tmp/eksctl /usr/local/bin
-eksctl version
-VALIDATE $? "eksctl installation"
+# curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+# tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+# sudo mv /tmp/eksctl /usr/local/bin
+# eksctl version
+# VALIDATE $? "eksctl installation"
 
 
 # kubectl
