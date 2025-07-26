51b57682d6cb49d2f7a6e47b6374cf0db3977207
prudviraj <prudvirajvemu316@gmail.com>
2025-07-26 10:59:09 +0530

    user profile

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
-# variable "sg_id" {
-#   type        = string
-#   default     = "sg-0a78c17f0e137e978"
-#   description = "description"
-# }
+variable "sg_id" {
+  type        = string
+  default     = "sg-0a78c17f0e137e978"
+  description = "description"
+}
 
-# variable "public_subnet_id" {
-#     type = string
-#     default = "subnet-0a6f144079a68fb90"
-# }
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-0a6f144079a68fb90"
+}
